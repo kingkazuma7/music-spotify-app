25c27c23a0ade6aa202173f9784848a46ce68bab
kingkazuma7 <your.nasionasio7@gmail.com>
2024-07-26 17:34:31 +0900

    27. クリックして曲を再生
    - setSelectedSong(song); 選択された曲を状態変数にセット
    - 選択されたpreview_urlをaudioタグのsrcにセット

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SongList } from './components/SongList';
 import './index.css';
 import spotify from './lib/spotify';
@@ -8,6 +8,11 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   // 曲の情報を格納
   const [popularSongs, setPopularSongs] = useState([]);
+  // 音楽の再生
+  const [isPlay, setIsPlay] = useState(false);
+  // 選択した曲の情報
+  const [selectedSong, setSelectedSong] = useState();
+  const audioRef = useRef(null);
 
   // apiから曲データ取得(確認用)
   useEffect(() => {
@@ -23,6 +28,16 @@ export default function App() {
     setPopularSongs(popularSongs);
     setIsLoading(false);
   }
+
+  const handleSongSelected = async (song) => {
+    setSelectedSong(song);
+    audioRef.current.src = song.preview_url;
+    // console.log(audioRef.current.src);
+    audioRef.current.play();
+    setIsPlay(true);
+    // console.log('handleSongSelectedをクリック');
+  }
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-500 text-white">
       <main className="flex-1 p-8 mb-20">
@@ -31,9 +46,10 @@ export default function App() {
         </header>
         <section>
           <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
-          <SongList isLoading={isLoading} songs={popularSongs} />
+          <SongList isLoading={isLoading} songs={popularSongs} onSongSelected={handleSongSelected} />
         </section>
       </main>
+      <audio ref={audioRef} />
     </div>
   );
 }
